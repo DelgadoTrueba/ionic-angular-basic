df3ccd3e62c587010d5149939fa046ac7c522ab4
delgado.trueba <delgado.trueba@outlook.es>
2019-06-29 08:39:15 +0200

    recipes page

---
+++ src/app/recipes/recipes.model.ts
@@ -0,0 +1,6 @@
+export interface Recipe {
+    id: string,
+    tittle: string,
+    imageUrl: string,
+    ingredients: string[]
+}
--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: "r1",
+      tittle: "chele",
+      imageUrl: "/assets/img/salad.jpg",
+      ingredients: ["French Fries", "Pork meat", "Salad"]
+    },
+    {
+      id: "r2",
+      tittle: "chele2",
+      imageUrl: "/assets/img/salad.jpg",
+      ingredients: ["French Fries", "Pork meat", "Salad"]
+    }
+  ]
 
   constructor() { }
 
